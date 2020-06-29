6c7e73b85834ce0e97f8fc162fa392c214c0c550
Jakob Clements <jakobclements95@gmail.com>
2020-06-29 14:21:28 -0400

    Create skeleton pages with functioning routes

--- COMP2068-Assignment-1/routes/index.js
+++ COMP2068-Assignment-1/routes/index.js
@@ -7,4 +7,24 @@ router.get('/', function (req, res) {
     res.render('index', { title: 'Express' });
 });
 
+/* GET about page. */
+router.get('/about', function (req, res) {
+    res.render('about', { title: 'About me' });
+});
+
+/* GET projects page. */
+router.get('/projects', function (req, res) {
+    res.render('projects', { title: 'Projects' });
+});
+
+/* GET services page. */
+router.get('/services', function (req, res) {
+    res.render('services', { title: 'Services' });
+});
+
+/* GET contact page. */
+router.get('/contact', function (req, res) {
+    res.render('contact', { title: 'Contact Me' });
+});
+
 module.exports = router;
